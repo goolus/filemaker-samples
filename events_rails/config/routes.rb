7137ea45a206f26fe9da7756ab27613d0d76d43b
ogusa <ogusa@goo-us.com>
2018-09-10 11:36:44 +0900

    events delete pathの変更

--- events_rails/config/routes.rb
+++ events_rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :show, :create, :destroy]
+  resources :events, only: [:index, :show, :create]
+  delete 'events', to: 'events#destroy'
   resources :organizers , only: [:index, :show, :create, :destroy]
 end
